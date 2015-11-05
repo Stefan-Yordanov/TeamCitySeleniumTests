37fe39e30d8858589be6084ee70b5f242b91d346
Stefan-Yordanov <stefan.yordanov@bedegaming.com>
2015-11-05 13:54:32 +0200

    Sel added

--- SeleniumTest/UnitTest1.cs
+++ SeleniumTest/UnitTest1.cs
@@ -12,19 +12,19 @@ namespace SeleniumTest
         [Test]
         public void SeleniumTets()
         {
-            //using (var driver = new FirefoxDriver())
-            //{
-            //    driver.Navigate().GoToUrl("http://localhost:5956");
-            //    driver.FindElements(By.TagName("a"))
-            //        .First(a => a.Text.Equals("About", StringComparison.InvariantCultureIgnoreCase)).Click();
-            //    Assert.AreEqual(driver.Url, "http://localhost:5956/Home/About");
-            //}
+            using (var driver = new FirefoxDriver())
+            {
+                driver.Navigate().GoToUrl("http://localhost:5956");
+                driver.FindElements(By.TagName("a"))
+                    .First(a => a.Text.Equals("About", StringComparison.InvariantCultureIgnoreCase)).Click();
+                Assert.AreEqual(driver.Url, "http://localhost:5956/Home/About");
+            }
         }
 
         [Test]
         public void SimpleTest()
         {
-            Assert.AreEqual(1,2);
+            Assert.AreEqual(1,1);
         }
     }
 }
