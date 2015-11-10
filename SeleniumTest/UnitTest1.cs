f87382352285eb7491de02c4c4dd32faf6eb47e0
Stefan-Yordanov <stefan.yordanov@bedegaming.com>
2015-11-10 02:19:52 +0200

    qew

--- SeleniumTest/UnitTest1.cs
+++ SeleniumTest/UnitTest1.cs
@@ -19,8 +19,10 @@ namespace SeleniumTest
             using (var driver = new FirefoxDriver(new FirefoxBinary("D:\\Program Files\\Mozilla Firefox\\firefox.exe"), new FirefoxProfile()))
             //using (var driver = new EdgeDriver())
             {
+                System.Threading.Thread.Sleep(2000);
                 driver.Navigate().GoToUrl("http://localhost:5956");
-                driver.Navigate().GoToUrl("http://localhost:5956");
+
+                //driver.Navigate().GoToUrl("http://localhost:5956");
                 driver.GetScreenshot().SaveAsFile("data.png",ImageFormat.Png);
                 System.IO.File.WriteAllText("Text.txt",driver.FindElement(By.TagName("innerHTML")).Text);
                 driver.FindElements(By.TagName("a")).First(a => a.Text.Equals("About", StringComparison.InvariantCultureIgnoreCase)).Click();
